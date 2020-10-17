0d453ab3b7e82010ca398f25c11a8d2361cffdd9
eddiemoonn <64956566+eddiemoonn@users.noreply.github.com>
2020-10-17 11:30:37 +0300

    Added codes to homepage

--- src/Homepage.java
+++ src/Homepage.java
@@ -5,6 +5,20 @@ public abstract class Homepage {
     String Social_media;
     String Menu;
     Homepage(){}
+    
+    public boolean post(String message) {
+        if (logIn(this.Username, this.password)) {
+            // Send the post data.
+            boolean result =  sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+    abstract boolean sendData(byte[] data);
+    abstract void logOut();
 
 
 }
